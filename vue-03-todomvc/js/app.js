d4108d6a2cf5e6ca9d18cb0eb25c91b9bc920b59
1164734537 <1164734537@qq.com>
2020-12-10 18:00:43 +0800

    引入vue框架，并实例化

--- vue-03-todomvc/js/app.js
+++ vue-03-todomvc/js/app.js
@@ -1,6 +1,8 @@
-(function (window) {
+(function (Vue) { //表示依赖了全局的Vue
 	'use strict';
-
+	var app = new Vue({
+		el:'#todoapp'
+	})
 	// Your starting point. Enjoy the ride!
 
-})(window);
+})(Vue);
